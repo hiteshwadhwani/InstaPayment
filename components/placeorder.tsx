68617e95e2a62c51056401a11cffb8424910b786
hiteshwadhwani <developerhitesh1403@gmail.com>
2024-03-14 02:10:28 +0530

    updated

--- components/placeorder.tsx
+++ components/placeorder.tsx
@@ -4,14 +4,16 @@ import { useBrandInfo } from "@/providers/brandinfo-provider"
 import Loading from "./ui/loading"
 import { Button } from "./ui/button"
 import { ArrowRight } from "lucide-react"
+import { useRouter } from "next/navigation"
 
 const PlaceorderButton = () => {
     const brandInfo = useBrandInfo()
+    const router = useRouter()
     if(!brandInfo){
         return <Loading className="h-[64px]" />
     }
     return (
-        <Button className="h-[64px] uppercase rounded-[6px] w-full text-white font-bold text-[14px] flex justify-between group p-[24px]" style={{backgroundColor: brandInfo.theme["--primary"]}}>
+        <Button onClick={() => router.push("/payment")} className="h-[64px] uppercase rounded-[6px] w-full text-white font-bold text-[14px] flex justify-between group p-[24px]" style={{backgroundColor: brandInfo.theme["--primary"]}}>
             place order
             <span>
                 <ArrowRight className="size-6 group-hover:translate-x-1 transition" />
